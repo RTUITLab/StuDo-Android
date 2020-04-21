e7eda208c02a226ad46c87be74255641c5f32394
Nikita <almostroll@yandex.ru>
2020-04-21 17:47:47 +0300

    Fixed theme on auth activity

--- app/src/main/java/com/rtuitlab/studo/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/rtuitlab/studo/ui/auth/AuthActivity.kt
@@ -1,13 +1,25 @@
 package com.rtuitlab.studo.ui.auth
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.rtuitlab.studo.R
 
 class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        paintStatusBar()
+    }
+
+    private fun paintStatusBar() {
+        if (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_NO){
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
     }
 }
